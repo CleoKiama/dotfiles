da091acf43bf9d2a6ef33d69f62472be2c4a7b01
CleoKiama <cleophaskiama7@gmail.com>
2025-05-09 10:03:53 +0300

    refactor(copilot): updated keymap

--- nvim/.config/nvim/lua/configs/copilot.lua
+++ nvim/.config/nvim/lua/configs/copilot.lua
@@ -5,7 +5,7 @@ require("copilot").setup({
 		hide_during_completion = true,
 		debounce = 75,
 		keymap = {
-			accept = "<M-j>",
+			accept = "<M-l>",
 			accept_word = false,
 			accept_line = false,
 			next = "<M-]>",
