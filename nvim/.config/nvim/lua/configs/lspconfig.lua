b68ade9900738a51a627df13a1cc20903667bce7
CleoKiama <cleophaskiama7@gmail.com>
2025-09-10 20:54:24 +0300

    chore(nvim) updated blick.cmp

--- nvim/.config/nvim/lua/configs/lspconfig.lua
+++ nvim/.config/nvim/lua/configs/lspconfig.lua
@@ -11,134 +11,132 @@ vim.env.PATH = table.concat({ vim.fn.stdpath("data"), "mason", "bin" }, sep) .. 
 
 -- Common attach function for all LSP servers
 M.on_attach = function(_, bufnr)
-  local function opts(desc)
-    return { buffer = bufnr, desc = "LSP: " .. desc }
-  end
-  -- Key mappings
-  map("n", "gD", function()
-    Snacks.picker.lsp_declarations()
-  end, opts("Go to declaration"))
-  map("n", "gd",
-    function()
-      Snacks.picker.lsp_definitions()
-    end
-    , opts("Go to definition"))
-  map("n", "gi", vim.lsp.buf.implementation, opts("Go to implementation"))
-  map("n", "gr", function()
-    Snacks.picker.lsp_references()
-  end, opts("Show references"))
-  map("n", "K", vim.lsp.buf.hover, opts("Show hover documentation"))
-  map("n", "<leader>sh", vim.lsp.buf.signature_help, opts("Show signature help"))
-  map("n", "<leader>ra", function()
-    require("nvchad.lsp.renamer")()
-  end, { desc = "Rename symbol" })
-  map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts("Code action"))
+	local function opts(desc)
+		return { buffer = bufnr, desc = "LSP: " .. desc }
+	end
+	-- Key mappings
+	map("n", "gD", function()
+		Snacks.picker.lsp_declarations()
+	end, opts("Go to declaration"))
+	map("n", "gd", function()
+		Snacks.picker.lsp_definitions()
+	end, opts("Go to definition"))
+	map("n", "gi", vim.lsp.buf.implementation, opts("Go to implementation"))
+	map("n", "gr", function()
+		Snacks.picker.lsp_references()
+	end, opts("Show references"))
+	map("n", "K", vim.lsp.buf.hover, opts("Show hover documentation"))
+	map("n", "<leader>sh", vim.lsp.buf.signature_help, opts("Show signature help"))
+	map("n", "<leader>ra", function()
+		require("nvchad.lsp.renamer")()
+	end, { desc = "Rename symbol" })
+	map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts("Code action"))
 
-  -- Workspace folder management
-  map("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts("Add workspace folder"))
-  map("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts("Remove workspace folder"))
-  map("n", "<leader>wl", function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, opts("List workspace folders"))
+	-- Workspace folder management
+	map("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts("Add workspace folder"))
+	map("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts("Remove workspace folder"))
+	map("n", "<leader>wl", function()
+		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+	end, opts("List workspace folders"))
 end
 
 -- Disable semantic tokens (optional)
 M.on_init = function(client, _)
-  if client.supports_method("textDocument/semanticTokens") then
-    client.server_capabilities.semanticTokensProvider = nil
-  end
+	if client.supports_method("textDocument/semanticTokens") then
+		client.server_capabilities.semanticTokensProvider = nil
+	end
 end
 
 M.capabilities = require("blink.cmp").get_lsp_capabilities()
 -- Setup default configurations
 M.setup = function()
-  -- Configure lua_ls with some defaults
-  lspconfig.lua_ls.setup({
-    on_attach = function(client, bufnr)
-      M.on_attach(client, bufnr)
-      navic.attach(client, bufnr)
-    end,
-    capabilities = M.capabilities,
-    on_init = M.on_init,
-    settings = {
-      Lua = {
-        diagnostics = {
-          globals = { "vim" },
-        },
-        workspace = {
-          library = {
-            vim.fn.expand("$VIMRUNTIME/lua"),
-            vim.fn.expand("$VIMRUNTIME/lua/vim/lsp"),
-          },
-          maxPreload = 100000,
-          preloadFileSize = 10000,
-        },
-      },
-    },
-  })
+	-- Configure lua_ls with some defaults
+	lspconfig.lua_ls.setup({
+		on_attach = function(client, bufnr)
+			M.on_attach(client, bufnr)
+			navic.attach(client, bufnr)
+		end,
+		capabilities = M.capabilities,
+		on_init = M.on_init,
+		settings = {
+			Lua = {
+				diagnostics = {
+					globals = { "vim" },
+				},
+				workspace = {
+					library = {
+						vim.fn.expand("$VIMRUNTIME/lua"),
+						vim.fn.expand("$VIMRUNTIME/lua/vim/lsp"),
+					},
+					maxPreload = 100000,
+					preloadFileSize = 10000,
+				},
+			},
+		},
+	})
 
-  -- Setup additional servers
-  local servers = {
-    "html",
-    "cssls",
-    "bashls",
-    "marksman",
-    "prismals",
-    "postgres_lsp",
-    -- "sqlls",
-    "gleam",
-    "jsonls",
-    "yamlls",
-  }
+	-- Setup additional servers
+	local servers = {
+		"html",
+		"cssls",
+		"bashls",
+		"marksman",
+		"prismals",
+		"postgres_lsp",
+		-- "sqlls",
+		"gleam",
+		"jsonls",
+		"yamlls",
+	}
 
-  for _, server in ipairs(servers) do
-    lspconfig[server].setup({
-      on_attach = function(client, bufnr)
-        M.on_attach(client, bufnr)
-        if client.name ~= "postgres_lsp" then
-          navic.attach(client, bufnr) -- Attach navic for all servers except postgres_lsp postgres_lsp not support
-        end
-      end,
-      capabilities = M.capabilities,
-      on_init = M.on_init,
-    })
-  end
-  -- Configure harper_ls for Obsidian vault markdown files only
-  lspconfig.harper_ls.setup({
-    filetypes = { "markdown" },
-    root_dir = function(fname)
-      -- Only activate on markdown files in the Obsidian vault
-      local vault_pattern = "^" .. vim.g.vault_path:gsub("%-", "%%-") .. "/.+%.md$"
-      if string.match(fname, vault_pattern) then
-        return vim.g.vault_path
-      end
-      return nil
-    end,
-    on_attach = M.on_attach,
-    on_init = M.on_init,
-    capabilities = M.capabilities,
-    settings = {
-      ["harper-ls"] = {
-        linters = {
-          SpellCheck = true,
-          SpelledNumbers = false,
-          AnA = true,
-          SentenceCapitalization = true,
-          UnclosedQuotes = true,
-          WrongQuotes = false,
-          LongSentences = true,
-          RepeatedWords = true,
-          Spaces = true,
-          Matcher = true,
-          CorrectNumberSuffix = true,
-        },
-        diagnosticSeverity = "hint",
-        markdown = {
-          IgnoreLinkTitle = false,
-        },
-      },
-    },
-  })
+	for _, server in ipairs(servers) do
+		lspconfig[server].setup({
+			on_attach = function(client, bufnr)
+				M.on_attach(client, bufnr)
+				if client.name ~= "postgres_lsp" then
+					navic.attach(client, bufnr) -- Attach navic for all servers except postgres_lsp postgres_lsp not support
+				end
+			end,
+			capabilities = M.capabilities,
+			on_init = M.on_init,
+		})
+	end
+	-- Configure harper_ls for Obsidian vault markdown files only
+	lspconfig.harper_ls.setup({
+		filetypes = { "markdown" },
+		root_dir = function(fname)
+			-- Only activate on markdown files in the Obsidian vault
+			local vault_pattern = "^" .. vim.g.vault_path:gsub("%-", "%%-") .. "/.+%.md$"
+			if string.match(fname, vault_pattern) then
+				return vim.g.vault_path
+			end
+			return nil
+		end,
+		on_attach = M.on_attach,
+		on_init = M.on_init,
+		capabilities = M.capabilities,
+		settings = {
+			["harper-ls"] = {
+				linters = {
+					SpellCheck = true,
+					SpelledNumbers = false,
+					AnA = true,
+					SentenceCapitalization = true,
+					UnclosedQuotes = true,
+					WrongQuotes = false,
+					LongSentences = true,
+					RepeatedWords = true,
+					Spaces = true,
+					Matcher = true,
+					CorrectNumberSuffix = true,
+				},
+				diagnosticSeverity = "hint",
+				markdown = {
+					IgnoreLinkTitle = false,
+				},
+			},
+		},
+	})
 end
 
 return M
--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -1,140 +1,146 @@
 return {
-	{
-		"saghen/blink.cmp",
-		version = "1.*",
-		dependencies = {
-			{ "L3MON4D3/LuaSnip", version = "v2.*" },
-			"disrupted/blink-cmp-conventional-commits",
-			"Kaiser-Yang/blink-cmp-avante",
-			"moyiz/blink-emoji.nvim",
-			{
-				"saghen/blink.compat",
-				opts = {},
-			},
-		},
-		event = { "InsertEnter" },
-		---@module 'blink.cmp'
-		---@type blink.cmp.Config
-		opts = {
-			cmdline = {
-				keymap = {
-					["<Tab>"] = { "accept" },
-				},
-				completion = {
-					menu = { auto_show = true },
-				},
-			},
-			completion = {
-				menu = { auto_show = true },
-				documentation = {
-					auto_show = true,
-				},
-			},
-			snippets = { preset = "luasnip" },
-			sources = {
-				default = {
-					"lsp",
-					"snippets",
-					"path",
-					"buffer",
-					"lazydev",
-					"avante",
-				},
-				per_filetype = {
-					sql = { "dadbod" },
-					git = { "conventional_commits" },
-					NeogitCommitMessage = { "conventional_commits" },
-					markdown = { "emoji", "snippets" },
-				},
-				providers = {
-					lsp = {
-						name = "LSP",
-						module = "blink.cmp.sources.lsp",
-						opts = {}, -- Passed to the source directly, varies by source
-						--- NOTE: All of these options may be functions to get dynamic behavior
-						--- See the type definitions for more information
-						enabled = true, -- Whether or not to enable the provider
-						async = false, -- Whether we should show the completions before this provider returns, without waiting for it
-						timeout_ms = 2000, -- How long to wait for the provider to return before showing completions and treating it as asynchronous
-						transform_items = nil, -- Function to transform the items before they're returned
-						should_show_items = true, -- Whether or not to show the items
-						max_items = 10, -- Maximum number of items to display in the menu
-						min_keyword_length = 0, -- Minimum number of characters in the keyword to trigger the provider
-						-- If this provider returns 0 items, it will fallback to these providers.
-						-- If multiple providers fallback to the same provider, all of the providers must return 0 items for it to fallback
-						fallbacks = {},
-						score_offset = 100, -- Boost/penalize the score of the items
-						override = nil, -- Override the source's functions
-					},
-					avante = {
-						module = "blink-cmp-avante",
-						name = "Avante",
-						opts = {
-							-- options for blink-cmp-avante
-						},
-					},
-					lazydev = {
-						name = "LazyDev",
-						module = "lazydev.integrations.blink",
-						score_offset = 85,
-						enabled = function()
-							return vim.bo.filetype == "lua"
-						end,
-					},
-					dadbod = {
-						name = "Dadbod",
-						module = "vim_dadbod_completion.blink",
-						min_keyword_length = 2,
-						score_offset = 80,
-					},
-					snippets = {
-						name = "snippets",
-						enabled = true,
-						max_items = 30,
-						min_keyword_length = 1,
-						module = "blink.cmp.sources.snippets",
-						score_offset = 95,
-					},
-					conventional_commits = {
-						name = "Conventional Commits",
-						module = "blink-cmp-conventional-commits",
-						enabled = function()
-							return vim.bo.filetype == "gitcommit"
-						end,
-						---@module 'blink-cmp-conventional-commits'
-						---@type blink-cmp-conventional-commits.Options
-						opts = {}, -- none so far
-					},
-					emoji = {
-						module = "blink-emoji",
-						name = "Emoji",
-						score_offset = 15,
-						opts = { insert = true },
-						should_show_items = function()
-							return vim.tbl_contains(
-								-- Enable emoji completion only for git commits and markdown.
-								-- By default, enabled for all file-types.
-								{ "gitcommit", "markdown" },
-								vim.o.filetype
-							)
-						end,
-					},
-				},
-			},
-			keymap = {
-				["C-u"] = { "snippet_forward", "fallback" },
-				["C-d"] = { "snippet_backward", "fallback" },
-				["<Up>"] = { "select_prev", "fallback" },
-				["<Down>"] = { "select_next", "fallback" },
-				["<C-p>"] = { "select_prev", "fallback" },
-				["<C-n>"] = { "select_next", "fallback" },
-				["<C-u>"] = { "scroll_documentation_up", "fallback" },
-				["<C-d>"] = { "scroll_documentation_down", "fallback" },
-				["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
-				["<C-y>"] = { "select_and_accept" },
-				["<CR>"] = { "select_and_accept", "fallback" },
-				["<C-e>"] = { "hide", "fallback" },
-			},
-		},
-	},
+  {
+    "saghen/blink.cmp",
+    version = "1.*",
+    dependencies = {
+      { "L3MON4D3/LuaSnip", version = "v2.*" },
+      "disrupted/blink-cmp-conventional-commits",
+      "moyiz/blink-emoji.nvim",
+      {
+        "saghen/blink.compat",
+        opts = {},
+      },
+    },
+    event = { "InsertEnter", "CmdLineEnter" },
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+      cmdline = {
+        keymap = {
+          ["<Tab>"] = { "accept" },
+        },
+        completion = {
+          menu = { auto_show = true },
+        },
+      },
+      completion = {
+        menu = { auto_show = true },
+        documentation = {
+          auto_show = true,
+        },
+      },
+      snippets = { preset = "luasnip" },
+      sources = {
+        default = {
+          "lsp",
+          "snippets",
+          "path",
+          "buffer",
+          "lazydev",
+        },
+        per_filetype = {
+          sql = { "dadbod" },
+          git = { "conventional_commits" },
+          NeogitCommitMessage = { "conventional_commits" },
+          markdown = { "emoji", "snippets" },
+        },
+        providers = {
+          lsp = {
+            name = "LSP",
+            module = "blink.cmp.sources.lsp",
+            opts = {}, -- Passed to the source directly, varies by source
+            --- NOTE: All of these options may be functions to get dynamic behavior
+            --- See the type definitions for more information
+            enabled = true,           -- Whether or not to enable the provider
+            async = false,            -- Whether we should show the completions before this provider returns, without waiting for it
+            timeout_ms = 2000,        -- How long to wait for the provider to return before showing completions and treating it as asynchronous
+            transform_items = nil,    -- Function to transform the items before they're returned
+            should_show_items = true, -- Whether or not to show the items
+            max_items = 20,           -- Maximum number of items to display in the menu
+            min_keyword_length = 0,   -- Minimum number of characters in the keyword to trigger the provider
+            -- If this provider returns 0 items, it will fallback to these providers.
+            -- If multiple providers fallback to the same provider, all of the providers must return 0 items for it to fallback
+            fallbacks = {},
+            score_offset = 0, -- Boost/penalize the score of the items
+            override = nil,   -- Override the source's functions
+          },
+          avante = {
+            module = "blink-cmp-avante",
+            name = "Avante",
+            opts = {
+              -- options for blink-cmp-avante
+            },
+          },
+          lazydev = {
+            name = "LazyDev",
+            module = "lazydev.integrations.blink",
+            score_offset = 85,
+            enabled = function()
+              return vim.bo.filetype == "lua"
+            end,
+          },
+          dadbod = {
+            name = "Dadbod",
+            module = "vim_dadbod_completion.blink",
+            min_keyword_length = 2,
+            score_offset = 80,
+          },
+          snippets = {
+            name = "snippets",
+            enabled = true,
+            max_items = 10,
+            min_keyword_length = 1,
+            module = "blink.cmp.sources.snippets",
+            score_offset = 0,
+          },
+          conventional_commits = {
+            name = "Conventional Commits",
+            module = "blink-cmp-conventional-commits",
+            enabled = function()
+              return vim.bo.filetype == "gitcommit"
+            end,
+            ---@module 'blink-cmp-conventional-commits'
+            ---@type blink-cmp-conventional-commits.Options
+            opts = {}, -- none so far
+          },
+          emoji = {
+            module = "blink-emoji",
+            name = "Emoji",
+            score_offset = 15,
+            opts = { insert = true },
+            should_show_items = function()
+              return vim.tbl_contains(
+              -- Enable emoji completion only for git commits and markdown.
+              -- By default, enabled for all file-types.
+                { "gitcommit", "markdown" },
+                vim.o.filetype
+              )
+            end,
+          },
+          opencode_context = {
+            name = "OpenCode Context",
+            module = "custom.opencode.completion_source",
+            enabled = function()
+              local buf_name = vim.api.nvim_buf_get_name(0)
+              return buf_name:match("opencode://") ~= nil
+            end,
+            score_offset = 1000,
+            min_keyword_length = 1,
+          },
+        },
+      },
+      keymap = {
+        ["<Up>"] = { "select_prev", "fallback" },
+        ["<Down>"] = { "select_next", "fallback" },
+        ["<C-p>"] = { "select_prev", "fallback" },
+        ["<C-n>"] = { "select_next", "fallback" },
+        ["<C-u>"] = { "scroll_documentation_up", "fallback" },
+        ["<C-d>"] = { "scroll_documentation_down", "fallback" },
+        ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+        ["<C-y>"] = { "select_and_accept" },
+        ["<CR>"] = { "select_and_accept", "fallback" },
+        ["<C-e>"] = { "hide", "fallback" },
+      },
+    },
+  },
 }
