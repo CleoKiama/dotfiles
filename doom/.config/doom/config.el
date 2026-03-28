36a74460b8cf437fa70c27dc2bf069b648035c34
CleoKiama <cleophaskiama7@gmail.com>
2026-03-28 18:00:05 +0300

    chore(doom-emacs): added contacts el and some few tweaks around templates

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -49,10 +49,13 @@
 (defvar my/org-notes    (concat my/org-root "/roam/"))
 (defvar my/org-archive  (concat my/org-root "/archive/"))
 (defvar my/org-templates (concat my/org-root "/templates/"))
+(defvar my/org-attachments (concat my/org-root "/attachments/")) 
 
 (setq org-directory (concat my/org-root "/"))
 
 (setq org-agenda-files (list my/org-gtd))
+
+(setq org-attach-id-dir my/org-attachments) 
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `with-eval-after-load' block, otherwise Doom's defaults may override your
@@ -97,16 +100,60 @@
 (after! org
   (setq org-agenda-files (list my/org-gtd (concat my/org-notes "journal/")))
   (setq org-capture-templates
-        '(("t" "Personal Todo" entry
-           (file+headline (concat my/org-gtd "inbox.org") "Tasks")
-           "* TODO %?\n  %i\n  %a" :prepend t)
-          ("m" "Meeting" entry
-           (file+headline (concat my/org-gtd "inbox.org") "Meetings")
-           "* MEETING with %? :MEETING:\n  %U" :prepend t)))
+      '(("t" "Personal Todo" entry
+         (file+headline (concat my/org-gtd "inbox.org") "Tasks")
+         "* TODO %?\n  %i\n  %a" :prepend t)
+        ("m" "Meeting" entry
+         (file+headline (concat my/org-gtd "inbox.org") "Meetings")
+         "* MEETING with %? :MEETING:\n  %U" :prepend t)
+        ("c" "Contact" entry                                      ;; add this
+         (file (concat my/org-gtd "contacts.org"))
+            "* %^{Name}
+            :PROPERTIES:
+            :EMAIL:    %^{Email}
+            :PHONE:    %^{Phone}
+            :BIRTHDAY: %^{Birthday (YYYY-MM-DD)}
+            :NOTE:     %^{Note}
+            :END:" :prepend t)
+            ))
 
    (setq org-archive-location (concat my/org-archive "%s_archive::"))
      ;; This tells Org: "When I archive something in 'projects.org', 
      ;; move it to '/data/org/GTD/archives/projects.org_archive'"
+
+
+      ;;TODO states
+     (setq org-todo-keywords
+      '((sequence "TODO(t)" "NEXT(n)" "WAITING(w)" "|" "DONE(d)")))
+
+    ;; LOG when a TODO is moved to the NEXT actionable state 
+    (defun log-todo-next-creation-date (&rest ignore)
+            "Log NEXT creation time in the property drawer under the key 'ACTIVATED'"
+                (when (and (string= (org-get-todo-state) "NEXT")
+                    (not (org-entry-get nil "ACTIVATED")))
+                        (org-entry-put nil "ACTIVATED" (format-time-string "[%Y-%m-%d]"))))
+            (add-hook 'org-after-todo-state-change-hook #'log-todo-next-creation-date)
+
+      ;; Set to `time` to automatically record a timestamp when a TODO item is marked as done.
+      (setq org-log-done 'time)
+
+     (setq org-tag-alist '(
+       (:startgroup . nil)
+       ("@home"     . ?h)
+       ("@work"     . ?w)
+       ("@computer" . ?c)
+       ("@phone"    . ?p)
+       ("@errands"  . ?e)
+       ("@email"    . ?m)
+       (:endgroup   . nil)
+       ("@waiting"  . ?W)
+       ("#deep"     . ?d)
+       ("#quick"    . ?q)
+       ("#low"      . ?l)
+    ))
+
+
+
 )
 
 ;; 2. Org-Roam & Zettelkasten Configuration (SPC n r)
@@ -135,6 +182,8 @@
            :target (file+head "people/${slug}.org" 
             "#+title: ${title}\n#+filetags: :people:\n:PROPERTIES:\n:CREATED:  %U\n:EMAIL:    \n:ROLE:     \n:MET:      \n:END:\n\n* Notes\n\n* Conversations\n\n* Ideas & Connections")
            :unnarrowed t)))
+
+      
   
      (setq org-roam-dailies-capture-templates
         '(("d" "default" entry
@@ -146,10 +195,15 @@
              :target (file+head "reviews/%<%Y-W%W>-weekly-review.org"
                             ":PROPERTIES:\n:CREATED: %U\n:END:\n#+title: Weekly Review %<%Y-W%W>\n#+filetags: :journal:review:")
               :unnarrowed t)))
-    (defun log-todo-next-creation-date (&rest ignore)
-            "Log NEXT creation time in the property drawer under the key 'ACTIVATED'"
-                (when (and (string= (org-get-todo-state) "NEXT")
-                    (not (org-entry-get nil "ACTIVATED")))
-                        (org-entry-put nil "ACTIVATED" (format-time-string "[%Y-%m-%d]"))))
-                    (add-hook 'org-after-todo-state-change-hook #'log-todo-next-creation-date)
- )
+
+)
+
+
+(after! org-contacts
+  (setq org-contacts-files (list (concat my/org-gtd "contacts.org")))
+  (setq org-contacts-files (list (concat my/org-gtd "contacts.org")))
+  (setq org-agenda-include-diary t)
+  (setq org-contacts-birthday-property "BIRTHDAY")
+)
+
+
