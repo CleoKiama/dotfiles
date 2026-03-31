df1eea648461edd2e026779ef73128cc90348bf7
CleoKiama <cleophaskiama7@gmail.com>
2026-03-31 21:21:30 +0300

    chore(nvim): don't need this anymore

--- nvim/.config/nvim/lua/plugins/ai.lua
+++ nvim/.config/nvim/lua/plugins/ai.lua
@@ -9,55 +9,10 @@ return {
 		end,
 	},
 	{
-		"folke/sidekick.nvim",
-		enabled = false,
-		event = "InsertEnter",
-		opts = {
-			-- add any options here
-		},
-		keys = {
-			{
-				"<tab>",
-				function()
-					-- if there is a next edit, jump to it, otherwise apply it if any
-					if not require("sidekick").nes_jump_or_apply() then
-						return "<Tab>" -- fallback to normal tab
-					end
-				end,
-				expr = true,
-				desc = "Goto/Apply Next Edit Suggestion",
-			},
-			{
-				"<localleader>aa",
-				function()
-					require("sidekick.cli").toggle({ focus = true })
-				end,
-				desc = "Sidekick Toggle CLI",
-				mode = { "n", "v" },
-			},
-			{
-				"<localleader>ao",
-				function()
-					require("sidekick.cli").toggle({ name = "opencode", focus = true })
-				end,
-				desc = "Sidekick Claude Toggle",
-				mode = { "n", "v" },
-			},
-		},
-	},
-	{
 		"NickvanDyke/opencode.nvim",
 		init = function()
 			-- Must be set before plugin loads
 			vim.g.opencode_opts = {
-				provider = {
-					enabled = "tmux",
-					tmux = {
-						options = "-h",
-						focus = false,
-						allow_passthrough = false,
-					},
-				},
 				prompts = {
 					ask_auto = { prompt = "", ask = true, submit = true },
 					ask_this = { prompt = "@this: ", ask = true, submit = true },
@@ -69,18 +24,6 @@ return {
 					test = { prompt = "Add tests for @this", submit = true },
 				},
 			}
-		end,
-		config = function()
-			-- Listen for opencode events
-			vim.api.nvim_create_autocmd("User", {
-				pattern = "OpencodeEvent",
-				callback = function(args)
-					-- Do something interesting, like show a notification when opencode finishes responding
-					if args.data.type == "session.idle" then
-						vim.notify("opencode finished responding")
-					end
-				end,
-			})
 		end,
 		keys = {
 			-- General ask commands
@@ -97,7 +40,7 @@ return {
 			{
 				"<leader>aq",
 				function()
-					require("opencode").ask("@this: ", {
+					require("opencode").ask(" ", {
 						submit = false,
 					})
 				end,
