4dd5facaad503a2b59a836f857ffadc7c1dd4e96
CleoKiama <111430806+CleoKiama@users.noreply.github.com>
2026-05-18 17:08:50 +0300

    chore(hyprland): move conf to latest lua config style

---
+++ hypr/.config/hypr/hyprland/env.lua
@@ -0,0 +1,12 @@
+hl.env("QT_IM_MODULE", "fcitx")
+hl.env("XMODIFIERS", "@im=fcitx")
+hl.env("SDL_IM_MODULE", "fcitx")
+hl.env("INPUT_METHOD", "fcitx")
+hl.env("QT_QPA_PLATFORM", "wayland")
+hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
+hl.env("QT_QPA_PLATFORMTHEME", "qt5ct")
+hl.env("QT_WAYLAND_DISABLE_WINDOWDECORATION", "1")
+hl.env("QT_AUTO_SCREEN_SCALE_FACTOR", "1")
+hl.env("XCURSOR_SIZE", "24")
+hl.env("HYPRCURSOR_SIZE", "24")
+
