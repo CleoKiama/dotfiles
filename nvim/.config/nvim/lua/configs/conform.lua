6f8c1a05e25fb563eb361b0a9c6818badcb9a88d
CleoKiama <111430806+CleoKiama@users.noreply.github.com>
2026-05-18 12:31:53 +0300

    chore(nvim): python formatter black

--- nvim/.config/nvim/lua/configs/conform.lua
+++ nvim/.config/nvim/lua/configs/conform.lua
@@ -1,51 +1,22 @@
-local function has_prettier_config()
-	local prettier_files = {
-		".prettierrc",
-		".prettierrc.json",
-		".prettierrc.yml",
-		".prettierrc.yaml",
-		".prettierrc.json5",
-		".prettierrc.cjs",
-		".prettierrc.js",
-		".prettier.config.js",
-		"prettier.config.js",
-		"prettier.config.mjs",
-	}
-
-	for _, file in ipairs(prettier_files) do
-		if vim.fn.filereadable(vim.fn.getcwd() .. "/" .. file) == 1 then
-			return true
-		end
-	end
-	return false
-end
-
-local function get_js_formatter()
-	if has_prettier_config() then
-		return { "prettierd" }
-	else
-		return { "biome" }
-	end
-end
-
 local options = {
 	formatters_by_ft = {
 		lua = { "stylua" },
-		css = { "prettier" },
-		html = { "prettier" },
-		typescript = get_js_formatter(),
-		typescriptreact = get_js_formatter(),
-		javascriptreact = get_js_formatter(),
-		javascript = get_js_formatter(),
-		jsx = get_js_formatter(),
-		json = get_js_formatter(),
-		yaml = { "prettier" },
-		graphql = { "prettier" },
-		markdown = { "prettierd" },
+		css = { "oxfmt" },
+		html = { "oxfmt" },
+		typescript = { "oxfmt" },
+		typescriptreact = { "oxfmt" },
+		javascriptreact = { "oxfmt" },
+		javascript = { "oxfmt" },
+		jsx = { "oxfmt" },
+		json = { "oxfmt" },
+		yaml = { "oxfmt" },
+		graphql = { "oxfmt" },
+		markdown = { "oxfmt" },
 		elixir = { "lsp" },
 		gleam = { "lsp" },
 		sql = { "sql_formatter" },
 		sh = { "beautysh" },
+		python = { "black" },
 		go = { "goimports" },
 		rust = { "rustfmt" },
 	},
