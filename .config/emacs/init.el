22d2679e896796f9b1e09919e199f3d098102b72
CleoKiama <111430806+CleoKiama@users.noreply.github.com>
2026-07-14 22:29:36 +0300

    feat(emacs): get me some doom themes

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -199,6 +199,25 @@
  (use-package doom-modeline
    :init (doom-modeline-mode 1))
 
+(use-package doom-themes
+  :custom
+  ;; Global settings (defaults)
+  (doom-themes-enable-bold t)   ; if nil, bold is universally disabled
+  (doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  ;; for treemacs users
+  (doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
+  :config
+  (load-theme 'doom-one t)
+
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+  ;; Enable custom neotree theme (nerd-icons must be installed!)
+  (doom-themes-neotree-config)
+  ;; or for treemacs users
+  (doom-themes-treemacs-config)
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
+
 
 (use-package rainbow-delimiters
   :hook (prog-mode . rainbow-delimiters-mode))
